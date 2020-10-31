9e625ef9118b5da084a761fb5ee13100d30a7c4b
Remo Fiorentino <remo-jose.casadiego@klarna.com>
2020-10-31 23:55:24 +0100

    feat: add rootpath and improve error messaging

--- src/gitToolbox.ts
+++ src/gitToolbox.ts
@@ -1,17 +1,23 @@
-import simpleGit, { SimpleGit } from 'simple-git';
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
 
-export const gitBox = async () => {
-    const git = simpleGit();
+const options: SimpleGitOptions = {
+    baseDir: process.cwd(),
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+};
+
+export const gitBox = async (rootPath: string = process.cwd()) => {
+    const git = simpleGit({ ...options, baseDir: rootPath });
     try {
         await git.status();
-    }catch(e){
-        console.error(`Ops something wrong happen initializing git, this usually happens when vscode cannot find git ${e}`);
+    } catch (e) {
+        console.error(`${e}`);
         return null;
     }
     return git;
 };
 
-export const checkStagedFiles = async (git:SimpleGit):Promise<boolean> => ((await git?.status())?.staged.length > 0);
+export const checkStagedFiles = async (git: SimpleGit): Promise<boolean> => ((await git?.status())?.staged.length > 0);
 
-export const commit = async (git:SimpleGit, head:string, body:string) => ((await git?.commit([head, body])));
+export const commit = async (git: SimpleGit, head: string, body: string) => ((await git?.commit([head, body])));
 
