84b109dfec7b71506a6ce43e2fadf9de095f5e27
Remo Fiorentino <remo-jose.casadiego@klarna.com>
2020-11-02 00:20:45 +0100

    feat: add base configuration with types

---
+++ src/baseConfig.ts
@@ -0,0 +1,56 @@
+export const config = {
+    types: [{
+        label: 'feat',
+        description: {
+            en: 'A new feature'
+        }
+    },
+    {
+        label: 'fix',
+        description: {
+            en: 'A bug fix'
+        }
+    },
+    {
+        label: 'build',
+        description: {
+            en: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)'
+        }
+    },
+    {
+        label: 'ci',
+        description: {
+            en: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)'
+        }
+    },
+    {
+        label: 'docs',
+        description: {
+            en: 'Documentation only changes'
+        }
+    },
+    {
+        label: 'perf',
+        description: {
+            en: 'A code change that improves performance'
+        }
+    },
+    {
+        label: 'refactor',
+        description: {
+            en: 'A code change that neither fixes a bug nor adds a feature'
+        }
+    },
+    {
+        label: 'style',
+        description: {
+            en: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
+        }
+    },
+    {
+        label: 'test',
+        description: {
+            en: 'Adding missing tests or correcting existing tests'
+        }
+    }]
+};
